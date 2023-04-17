c65bf30cac1b76039091070e99fc2f0477229c72
CamelliaV <1160933247@qq.com>
2023-04-17 11:18:36 +0800

    Configuring Backend

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 app.use(
   cors({
-    origin: 'https://camelliav.github.io/youtube-simple-clone/',
+    origin: 'https://camelliav.github.io/',
     credentials: true
   })
 )
